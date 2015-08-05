d5a8b4262678aec15d3f93f18e6c6adeb84cb806
Andrew Carmer <carmerandrew@gmail.com>
2015-08-05 12:01:03 -0600

    push all project

---
+++ public/client.js
@@ -0,0 +1,32 @@
+
+var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+var statusMessage   = document.getElementById('status-message');
+var voteTally       = document.getElementById('vote-tally');
+var buttons = document.querySelectorAll("#choices button");
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    socket.send('voteCast', this.innerText);
+  });
+}
+
+
+socket.on('statusMessage', function(message) {
+  statusMessage.innerText = message;
+});
+
+socket.on('userConnection', function(count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+
+socket.on('voteCount', function(votes) {
+    voteTally.innerText = "Vote Breakdown: "
+
+  for (vote in votes){
+    voteTally.appendChild( document.createTextNode(" " + vote + ": " + votes[vote]) )
+
+  }
+})
